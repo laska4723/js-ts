e6dbf48813d3058ad9285928b41f0c38f84e7f48
Sergei Rozanov <laska4723@gmail.com>
2025-08-31 20:14:11 +0300

    Создание проекта. Пакеты. Задача 1

--- src/main.js
+++ src/main.js
@@ -1,16 +1,6 @@
-import { sayHi, sayText, sum, power, tomatoPrice } from './functions.js';
+import chalk from 'chalk';
 
-sayHi('Анатолий');
-sayText('Привет');
-sum(4, 6, 5, 9, 3);
-power(2, 10);
-
-console.log();
-
-console.log(tomatoPrice(1)); // 450
-console.log(tomatoPrice(0.65)); // 292.5
-console.log(tomatoPrice(3.289)); // 1480.05
-
-console.log(tomatoPrice(0, 50)); // 0
-console.log(tomatoPrice(1, 22.5)); // 22.5
-console.log(tomatoPrice(14.96, 21.0007)); // 314.170472
+console.log(chalk.bgBlue.grey(new Date().toISOString()));
+console.log(chalk.bgBlack.green(new Date().toISOString()));
+console.log(chalk.bgGrey.red(new Date().toISOString()));
+console.log(chalk.bgGreen.blue(new Date().toISOString()));
