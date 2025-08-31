f0ff80a7227e9c6bed61a1eee9e0bf0023be490e
Sergei Rozanov <laska4723@gmail.com>
2025-08-31 21:22:09 +0300

    Строки. Задача 2

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { len } from './functions.js';
+import { len, func } from './functions.js';
 
 console.log(len('Привет!')); // 7
 console.log(len('Это очень длинная строка :)')); // 27
+
+console.log();
+
+console.log(func('МОЛОКО')); // МолокО
+console.log(func('картошка')); // КартошкА
+console.log(func('СтРаНнОе СлОвО')); // Странное словО
