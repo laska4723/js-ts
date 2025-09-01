d00d347fc2e47608ec668b274e76e3e79fd7c203
Sergei Rozanov <laska4723@gmail.com>
2025-09-01 18:56:02 +0300

    fix: Строки. Задача 2. Улучшили функцию func

--- src/functions.js
+++ src/functions.js
@@ -7,7 +7,7 @@ export const len = (text) => {
 export const func = (text) => {
   return (
     chalk.green(text.slice(0, 1).toUpperCase()) +
-    chalk.blue(text.slice(1, text.length - 1).toLowerCase()) +
-    chalk.green(text.slice(text.length - 1).toUpperCase())
+    chalk.blue(text.slice(1, -1).toLowerCase()) +
+    chalk.green(text.slice(-1).toUpperCase())
   );
 };
