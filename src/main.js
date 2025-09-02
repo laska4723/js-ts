93e9a0cadd5df20683b87e2bc12417fd4413336c
Sergei Rozanov <laska4723@gmail.com>
2025-09-02 18:30:03 +0300

    fix: Строки. Задача 4. Исправили вывод в main.js

--- src/main.js
+++ src/main.js
@@ -17,4 +17,6 @@ console.log(isStringStartsWith('Карго Корги', 'КАРГО ')); // true
 console.log(isStringStartsWith('Карго Корги', 'кор ')); // false
 console.log(isStringStartsWith('Ковёр', 'кова ')); // false
 
+console.log('_____________________________________________________');
+
 formatString('Анатолий', 17);
