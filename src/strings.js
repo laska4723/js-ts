fcaec85cd0589af96eee0813a41931516811b64b
Sergei Rozanov <laska4723@gmail.com>
2025-11-01 19:46:25 +0300

    fix: Строки. Задача 2 (Перенёс функцию из functions.js в strings.js)

--- src/strings.js
+++ src/strings.js
@@ -1,3 +1,13 @@
+import chalk from 'chalk';
+
 export const len = (text) => {
   return text.length;
+};
+
+export const func = (text) => {
+  return (
+    chalk.green(text.slice(0, 1).toUpperCase()) +
+    chalk.blue(text.slice(1, text.length - 1).toLowerCase()) +
+    chalk.green(text.slice(text.length - 1).toUpperCase())
+  );
 };
