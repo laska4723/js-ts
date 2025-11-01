f8347a33f9d54d5585cd6fd763259b4518237912
Sergei Rozanov <laska4723@gmail.com>
2025-11-01 20:07:05 +0300

    Числа. Задача 1

--- src/main.js
+++ src/main.js
@@ -1,26 +1,3 @@
-import { len, func, isStringStartsWith, formatString, logStringInfo } from './strings.js';
+import { generateInteger } from './numbers.js';
 
-console.log(len('Привет!')); // 7
-console.log(len('Это очень длинная строка :)')); // 27
-
-console.log();
-
-console.log(func('МОЛОКО')); // МолокО
-console.log(func('картошка')); // КартошкА
-console.log(func('СтРаНнОе СлОвО')); // Странное словО
-
-console.log();
-
-console.log(isStringStartsWith('Ветеринар', 'вет')); // true
-console.log(isStringStartsWith('Молоко', 'мол')); // true
-console.log(isStringStartsWith('Карго Корги', 'КАРГО ')); // true
-console.log(isStringStartsWith('Карго Корги', 'кор ')); // false
-console.log(isStringStartsWith('Ковёр', 'кова ')); // false
-
-console.log('_____________________________________________________');
-
-formatString('Анатолий', 17);
-
-console.log('_____________________________________________________');
-
-logStringInfo('Это строка');
+console.log(generateInteger(1, 10));
